f246b16ec999735808cd17776b9e35c33674db48
SreevaniDhulipala24097 <sreevanidhulipala24097@yahoo.com>
2019-07-13 19:58:51 +0530

    Database updated

--- loop.sql
+++ loop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2019 at 01:24 PM
+-- Generation Time: Jul 13, 2019 at 03:33 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -36,17 +36,35 @@ CREATE TABLE `admin` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cluster`
+--
+
+CREATE TABLE `cluster` (
+  `cid` int(200) NOT NULL,
+  `clstrname` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cluster`
+--
+
+INSERT INTO `cluster` (`cid`, `clstrname`) VALUES
+(1, 'one'),
+(2, 'two');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `resource`
 --
 
 CREATE TABLE `resource` (
-  `rid` int(100) NOT NULL,
   `sid` int(100) NOT NULL,
-  `available` int(100) NOT NULL,
+  `available` tinyint(1) NOT NULL,
   `verified` int(100) NOT NULL,
   `rdescription` varchar(2000) NOT NULL,
-  `email` varchar(2000) NOT NULL,
-  `Repicture` blob NOT NULL
+  `Repicture` blob NOT NULL,
+  `rname` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -59,11 +77,20 @@ CREATE TABLE `school` (
   `sid` int(200) NOT NULL,
   `sname` varchar(2000) NOT NULL,
   `pname` varchar(200) NOT NULL,
-  `cno` bigint(20) NOT NULL,
+  `cno` varchar(200) NOT NULL,
   `saddress` varchar(2000) NOT NULL,
   `ppassword` varchar(200) NOT NULL,
-  `slpicture` blob NOT NULL
+  `slpicture` blob NOT NULL,
+  `schoolDes` varchar(2000) NOT NULL,
+  `schoolEmail` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `school`
+--
+
+INSERT INTO `school` (`sid`, `sname`, `pname`, `cno`, `saddress`, `ppassword`, `slpicture`, `schoolDes`, `schoolEmail`) VALUES
+(1, 'School1', 'school1', 'two', 'jpmc,Hyd', 'school1', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -91,6 +118,22 @@ CREATE TABLE `teachers` (
   `phone` bigint(15) NOT NULL,
   `subject` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `resource`
+--
+ALTER TABLE `resource`
+  ADD PRIMARY KEY (`rname`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`sid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
