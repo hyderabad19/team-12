885fa5def22a366df495a4b6b646a1c41fc1684f
SreevaniDhulipala24097 <sreevanidhulipala24097@yahoo.com>
2019-07-13 21:09:18 +0530

    Registration with Backend

--- Regis.php
+++ Regis.php
@@ -1,8 +1,28 @@
+<?php 
+$con=mysqli_connect("localhost","root","","loop");
+if(isset($_POST['button_in']))
+{
+$name=$_POST['sname'];
+$pname=$_POST['pname'];
+$saddress=$_POST['saddress'];
+$password=$_POST['ppassword'];
+$slpicture=$_POST['slpicture'];
+$schoolDes=$_POST['schoolDes'];
+$schoolEmail=$_POST['schoolEmail'];
+$password=md5($password);
+$sql=" INSERT INTO school (sname,pname,saddress,ppassword,slpicture,schoolDes,schoolEmail) VALUES $name,$pname,$saddress,$password,$slpicture,$schoolDes,$schoolEmail ";
+$verify=mysqli_query($con,$sql);
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="mystyle.css">
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
 <div class="container" id="container">
@@ -19,10 +39,11 @@
       <br>
       School Name:<input type=text placeholder="school name" name="sname" />
       Principal Name:<input type=text placeholder="Principal name" name="pname" />
-      Cluster No:<input type=text placeholder="cluster no" name="cno" />
       School Address:<textArea rows="4" cols="100" name="saddress" ></textArea>
-      Password:<input type=text placeholder="Password" name="ppassword" />
+      Password:<input type="password" placeholder="Password" name="ppassword" />
+      School email <input type="email" name="schoolEmail">
       Upload Image<input type=file name="slpicture">
+      School Description <textarea rows="4" cols="100" name="schoolDes"></textarea>>
       <button name="button_in">Sign In</button>
     </form>
   </div>
